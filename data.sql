9c0816e2c569a74d242030cdcd51a4ac16375748
sinulingga23 <sinulinggatwo@gmail.com>
2023-04-01 12:39:11 +0700

    update schema & add data

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO partner.m_field_type
+    (id, name) 
+VALUES
+    ('85e487ae-a38d-4c2b-9253-53681124b56b','Numeric'),
+    ('da47873e-e810-43c4-b038-54efaba1a86a','Alpha Numeric/Shor Text'),
+    ('6668cc4e-21e3-4a6e-be40-50cb324dc266','Paragraph'),
+    ('a1d17fd3-208b-42d7-8e71-9f495604261c','Radio Button'),
+    ('997dee50-2791-428a-bd51-06274213c9f4','Checkbox'),
+    ('5598bc8a-711f-4b2c-b44d-cf8c9d9652ea','Dropdown'),
+    ('8df9169c-ae3a-4c99-a528-f28383bdfcc1','Upload Image'),
+    ('cc996e23-1aa3-4c2a-8af4-661669b2d9ce','Date'),
+    ('a5a7f6d5-0f69-46b1-98dc-d86036582005','Time');
--- schema.sql
+++ schema.sql
@@ -27,3 +27,40 @@ create table partner.m_field_type (
     created_at timestamp not null default current_timestamp,
     updated_at timestamp null
 );
+
+create table partner.m_form_field (
+    id uuid not null primary key,
+    name varchar(150) not null,
+    m_form_id uuid not null,
+    m_form_type_id uuid not null,
+    is_mandatory boolean null,
+    is_should_answered boolean null,
+    ordering integer not null,
+    placeholder varchar(150) not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp null,
+    foreign key (m_form_id) references partner.m_form (id)
+    on update cascade on delete restrict,
+    foreign key (m_form_type_id) references partner.m_form_field (id)
+    on update cascade on delete restrict
+);
+
+create table partner.m_form_field_childs (
+    id uuid not null primary key,
+    name varchar(150) not null,
+    m_form_field_id uuid not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp null,
+    foreign key (m_form_field_id) references partner.m_form_field (id)
+    on update cascade on delete restrict
+);
+
+create table partner.m_form_field_answer (
+    id uuid not null primary key,
+    name varchar(150) not null,
+    m_form_field_childs_id uuid not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp null,
+    foreign key (m_form_field_childs_id) references partner.m_form_field_childs (id)
+    on update cascade on delete restrict
+);
